e0ae9722706ed01178a741e1fee06b06723729b1
xhad <chad@yieldnest.finance>
2024-09-10 20:44:23 +0800

    Arrage tests for unit and integration

---
+++ test/single/unit/access.t.sol
@@ -0,0 +1,71 @@
+// SPDX-License-Identifier: BSD-3-Clauses
+pragma solidity ^0.8.24;
+
+import "lib/forge-std/src/Test.sol";
+import {AccessControlUpgradeable, IAccessControl, IERC20, IAccessControl} from "src/Common.sol";
+import {LocalActors} from "script/Actors.sol";
+import {SingleVault, ISingleVault} from "src/SingleVault.sol";
+import {TestConstants} from "test/helpers/Constants.sol";
+import {MockERC20} from "test/mocks/MockERC20.sol";
+import {DeployFactory, VaultFactory} from "test/helpers/DeployFactory.sol";
+
+contract AccessControlTest is Test, LocalActors, TestConstants {
+    SingleVault public vault;
+    IERC20 public asset;
+
+    function setUp() public {
+        vm.startPrank(ADMIN);
+        asset = IERC20(address(new MockERC20(ASSET_NAME, ASSET_SYMBOL)));
+
+        DeployFactory deployFactory = new DeployFactory();
+        VaultFactory factory = deployFactory.deploy(0);
+        asset.approve(address(factory), 1 ether);
+        asset.transfer(address(factory), 1 ether);
+        address vaultAddress = factory.createSingleVault(
+            asset,
+            VAULT_NAME,
+            VAULT_SYMBOL,
+            ADMIN,
+            0, // time delay
+            deployFactory.getProposers(),
+            deployFactory.getExecutors()
+        );
+        vault = SingleVault(payable(vaultAddress));
+    }
+
+    function testAdminRoleSet() public view {
+        assertTrue(vault.hasRole(vault.DEFAULT_ADMIN_ROLE(), ADMIN));
+    }
+
+    function testAdminCanGrantRole() public {
+        vm.startPrank(ADMIN);
+        vault.grantRole(vault.DEFAULT_ADMIN_ROLE(), address(1));
+        assertEq(vault.hasRole(vault.DEFAULT_ADMIN_ROLE(), address(1)), true);
+    }
+
+    function skip_testNonAdminCannotGrantRole() public {
+        vm.startPrank(UNAUTHORIZED);
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                IAccessControl.AccessControlUnauthorizedAccount.selector, UNAUTHORIZED, vault.DEFAULT_ADMIN_ROLE()
+            )
+        );
+        vault.grantRole(vault.DEFAULT_ADMIN_ROLE(), address(4));
+    }
+
+    function testAdminCanRevokeRole() public {
+        vm.startPrank(ADMIN);
+        vault.revokeRole(vault.DEFAULT_ADMIN_ROLE(), address(1));
+        assertFalse(vault.hasRole(vault.DEFAULT_ADMIN_ROLE(), address(1)));
+    }
+
+    function skip_testNonAdminCannotRevokeRole() public {
+        vm.startPrank(UNAUTHORIZED);
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                IAccessControl.AccessControlUnauthorizedAccount.selector, UNAUTHORIZED, vault.DEFAULT_ADMIN_ROLE()
+            )
+        );
+        vault.revokeRole(vault.DEFAULT_ADMIN_ROLE(), address(1));
+    }
+}
---
+++ test/single/unit/initialize.t.sol
@@ -0,0 +1,62 @@
+// SPDX-License-Identifier: BSD-3-Clauses
+pragma solidity ^0.8.24;
+
+import "lib/forge-std/src/Test.sol";
+import {SingleVault} from "src/SingleVault.sol";
+import {IERC20, TransparentUpgradeableProxy} from "src/Common.sol";
+import {MockERC20} from "test/mocks/MockERC20.sol";
+import {LocalActors} from "script/Actors.sol";
+import {TestConstants} from "test/helpers/Constants.sol";
+import {DeployFactory, VaultFactory} from "test/helpers/DeployFactory.sol";
+import {SingleVault, ISingleVault} from "src/SingleVault.sol";
+
+contract InitializeTest is Test, LocalActors, TestConstants {
+    SingleVault public vault;
+    DeployFactory public deployFactory;
+    VaultFactory public factory;
+    IERC20 public asset;
+
+    function setUp() public {
+        vm.startPrank(ADMIN);
+        asset = IERC20(address(new MockERC20(ASSET_NAME, ASSET_SYMBOL)));
+        deployFactory = new DeployFactory();
+        factory = deployFactory.deploy(0);
+        asset.approve(address(factory), 1 ether);
+        asset.transfer(address(factory), 1 ether);
+        address vaultAddress = factory.createSingleVault(
+            asset,
+            VAULT_NAME,
+            VAULT_SYMBOL,
+            ADMIN,
+            0, // time delay
+            deployFactory.getProposers(),
+            deployFactory.getExecutors()
+        );
+        vault = SingleVault(payable(vaultAddress));
+    }
+
+    function testInitialize() public {
+        SingleVault vaultImplementation = new SingleVault();
+        TransparentUpgradeableProxy proxy = new TransparentUpgradeableProxy(
+            address(vaultImplementation),
+            address(this),
+            abi.encodeWithSignature(
+                "initialize(address,string,string,address,uint256,address[],address[])",
+                asset,
+                VAULT_NAME,
+                VAULT_SYMBOL,
+                ADMIN,
+                0,
+                deployFactory.getProposers(),
+                deployFactory.getExecutors()
+            )
+        );
+
+        ISingleVault newVault = ISingleVault(address(proxy));
+
+        assertEq(newVault.asset(), address(asset));
+        assertEq(newVault.hasRole(vault.DEFAULT_ADMIN_ROLE(), ADMIN), true);
+        assertEq(newVault.symbol(), VAULT_SYMBOL);
+        assertEq(newVault.name(), VAULT_NAME);
+    }
+}
