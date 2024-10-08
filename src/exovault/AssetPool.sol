6ebd116fcf11961239e14d5132fad7b2220ff9ee
danoctavian <danoctavian91@gmail.com>
2024-10-09 02:17:05 +0300

    don't implement rewards distribution yet

--- src/exovault/AssetPool.sol
+++ src/exovault/AssetPool.sol
@@ -21,17 +21,26 @@ contract AssetPool is Initializable, AccessControlUpgradeable {
     YnAsset public ynAsset;
     IRateProvider public rateProvider;
     mapping(address => Asset) public assets;
-    address[] public assetList;
+    IERC20[] public assetList;
+    IERC20[] public strategyList;
+    address public exoVault;
 
-    function initialize(YnAsset _ynAsset, IRateProvider _rateProvider) public initializer {
+    function initialize(YnAsset _ynAsset, IRateProvider _rateProvider, address _exoVault) public initializer {
         ynAsset = _ynAsset;
         rateProvider = _rateProvider;
+        exoVault = _exoVault;
     }
 
-    function addAsset(IERC20 _token) external onlyRole(ASSET_ADMIN_ROLE) {
-        require(!assets[address(_token)].active, "Asset already added");
-        assets[address(_token)] = Asset(true);
-        assetList.push(address(_token));
+    function addAsset(IERC20 asset) external onlyRole(ASSET_ADMIN_ROLE) {
+        require(!assets[address(asset)].active, "Asset already added");
+        assets[address(asset)] = Asset(true);
+        assetList.push(asset);
+    }
+
+    function addStrategy(IERC20 strategy) external onlyRole(ASSET_ADMIN_ROLE) {
+        require(!assets[address(strategy)].active, "Strategy already added");
+        assets[address(strategy)] = Asset(true);
+        strategyList.push(strategy);
     }
 
     function deposit(address asset, uint256 _amount, address _receiver) external {
@@ -41,4 +50,31 @@ contract AssetPool is Initializable, AccessControlUpgradeable {
         uint256 ynAssetAmount = rateProvider.convert(asset, _amount);
         ynAsset.mint(_receiver, ynAssetAmount);
     }
+
+    function mintRewards() external onlyRole(ASSET_ADMIN_ROLE) {
+        uint256 totalValue = 0;
+
+        // Calculate total value of assets
+        for (uint256 i = 0; i < assetList.length; i++) {
+            IERC20 asset = assetList[i];
+            uint256 balance = asset.balanceOf(address(this));
+            totalValue += rateProvider.convert(address(asset), balance);
+        }
+
+        // Calculate total value of strategies
+        for (uint256 i = 0; i < strategyList.length; i++) {
+            IERC20 strategy = strategyList[i];
+            uint256 balance = strategy.balanceOf(address(this));
+            totalValue += rateProvider.convert(address(strategy), balance);
+        }
+
+        // Calculate the amount of ynAsset to mint
+        uint256 currentSupply = ynAsset.totalSupply();
+        if (totalValue > currentSupply) {
+            uint256 amountToMint = totalValue - currentSupply;
+            ynAsset.mint(exoVault, amountToMint);
+        }
+    }
+
+    // TODO: add async redemption logic or YnAsset
 }
