9b083d64ff24a91817b921f78abc911b4f45adbe
MrDeadCe11 <mister.deadce11@gmail.com>
2024-12-17 19:44:51 -0600

    moved vault to function arg and removed initialization

--- src/utils/XReferralAdapter.sol
+++ src/utils/XReferralAdapter.sol
@@ -3,46 +3,47 @@
 pragma solidity ^0.8.24;
 
 import {IVault} from "src/BaseVault.sol";
-import {Initializable} from "src/Common.sol";
-import {Initializable} from "lib/openzeppelin-contracts-upgradeable/contracts/proxy/utils/Initializable.sol";
-import {AccessControlUpgradeable} from
-    "lib/openzeppelin-contracts-upgradeable/contracts/access/AccessControlUpgradeable.sol";
 
-contract XReferralAdapter is Initializable {
-    IVault public vault;
+contract XReferralAdapter {
 
     event ReferralDepositProcessed(
         address indexed vault,
         address indexed asset,
         address indexed depositor,
+        address indexed referrer,
         address indexed receiver,
         uint256 amount,
         uint256 shares,
-        address indexed referrer,
         uint256 timestamp
     );
 
     error InvalidVault(address);
     error ZeroAmount();
+    error ZeroAddress();
+    error SelfReferral();
 
     constructor() public {
-        _disableInitializers();
-    }
 
-    function initialize(address _vault) public initializer {
-        vault = IVault(_vault);
-        if (IVault(vault).asset() == address(0)) {
-            revert InvalidVault(_vault);
-        }
     }
 
     /**
-     * @dev wraps the deposit for the specific strategy to include a referral id for the desiered refererr
+     * @dev wraps the deposit for the specific strategy to emit a referal event
+     * @param _vault the vault to be used
+     * @param asset asset the ERC20 being deposited
+     * @param amount the amount of the asset being deposited
+     * @param referrer the address of the referrer
+     * @param receiver the addres of the receiver
+     * @return shares the shares being received
      */
-    function depositAssetWithReferral(address asset, uint256 amount, address referrer, address receiver)
+    function depositAssetWithReferral(address _vault, address asset, uint256 amount, address referrer, address receiver)
         public
         returns (uint256 shares)
     {
+        IVault vault = IVault(_vault);
+
+        if (IVault(vault).asset() == address(0)) {
+            revert InvalidVault(_vault);
+        }
         if (amount == 0) {
             revert ZeroAmount();
         }
@@ -59,7 +60,7 @@ contract XReferralAdapter is Initializable {
         shares = vault.depositAsset(asset, amount, receiver);
 
         emit ReferralDepositProcessed(
-            address(vault), asset, msg.sender, receiver, amount, shares, referrer, block.timestamp
+            address(vault), asset, msg.sender, referrer, receiver, amount, shares, block.timestamp
         );
     }
 
