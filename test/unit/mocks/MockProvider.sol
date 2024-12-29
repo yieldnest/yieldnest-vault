604e7d41387500b7554c2423a630c1b73cab04bb
danoctavian <danoctavian91@gmail.com>
2024-12-29 18:51:32 +0200

    add mock wip for mockprovider

--- test/unit/mocks/MockProvider.sol
+++ test/unit/mocks/MockProvider.sol
@@ -6,15 +6,25 @@ import {Provider} from "src/module/Provider.sol";
 contract MockProvider is Provider {
     mapping(address => uint256) private _mockRates;
 
+    mapping(address => bool) private _erc4626;
+
     function setRate(address asset, uint256 rate_) external {
         _mockRates[asset] = rate_;
     }
 
     function getRate(address asset) public view override returns (uint256) {
+        if (_erc4626[asset]) {
+            return IERC4626(asset).convertToAssets(1e18);
+        }
+
         uint256 mockRate = _mockRates[asset];
         if (mockRate != 0) {
             return mockRate;
         }
         return super.getRate(asset);
+    }
+
+    function addERC4626(address asset) external {
+        _erc4626[asset] = true;
     }
 }
