5044f571fef4d9ee3cac14b8811a289cc3f3bdd9
Dan OneTree <dan13ram@gmail.com>
2025-01-12 17:03:15 +0530

    support asBNB in provider

--- script/Contracts.sol
+++ script/Contracts.sol
@@ -17,10 +17,13 @@ library MainnetContracts {
     address public constant WBNB = 0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c;
     address public constant SLISBNB = 0xB0b84D294e0C75A6abe60171b70edEb2EFd14A1B;
     address public constant BNBX = 0x1bdd3Cf7F79cfB8EdbB955f20ad99211551BA275;
+    address public constant CLISBNB = 0x4b30fcAA7945fE9fDEFD2895aae539ba102Ed6F6;
+    address public constant ASBNB = 0x77734e70b6E88b4d82fE632a168EDf6e700912b6;
 
     // stake managers
     address public constant BNBX_STAKE_MANAGER = 0x3b961e83400D51e6E1AF5c450d3C7d7b80588d28;
     address public constant SLIS_BNB_STAKE_MANAGER = 0x1adB950d8bB3dA4bE104211D5AB038628e477fE6;
+    address public constant AS_BNB_MINTER = 0x2F31ab8950c50080E77999fa456372f276952fD8;
 
     // max vault
     address public constant YNBNBX = 0x32C830f5c34122C6afB8aE87ABA541B7900a2C5F;
@@ -29,6 +32,7 @@ library MainnetContracts {
     address public constant YNBNBK = 0x304B5845b9114182ECb4495Be4C91a273b74B509;
     address public constant YNWBNBK = 0x6EC6b7F106674d6D82b7b24446C7ebaf349d59A1;
     address public constant YNCLISBNBK = 0x03276919F8b6eE37BA8EE4ee68a1c5f48b667834;
+    address public constant YNASBNBK = address(0x0a); // TODO: Update with deployed address
 
     address public constant PROVIDER = address(0x0d); // TODO: Update with deployed Provider
     address public constant BUFFER = 0x6EC6b7F106674d6D82b7b24446C7ebaf349d59A1;
