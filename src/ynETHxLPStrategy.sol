cd291dd40d86dd9ca3e5fc1452a46537722faefa
MrDeadCe11 <mister.deadce11@gmail.com>
2025-01-23 01:47:13 -0600

    fixed setup vault

--- src/ynETHxLPStrategy.sol
+++ src/ynETHxLPStrategy.sol
@@ -1,14 +1,21 @@
 // SPDX-License-Identifier: BSD-3-Clause
 pragma solidity ^0.8.24;
 
-import {Strategy} from "src/Strategy.sol";
+import {BaseStrategy} from "src/strategy/yearn/BaseStrategy.sol";
 
-contract ynETHxLPStrategy is Strategy {
+contract YNETHxLPStrategy is BaseStrategy {
+    
+    constructor(address _lpToken, string memory _name) BaseStrategy(_lpToken, _name) {}
 
-    // function _convertToAssets(uint256 shares) internal view override returns (uint256 assets) {
-    //     assets = shares * IERC4626(MC.YNLSDE).convertToAssets(1e18) / IERC4626(MC.YNETH).convertToAssets(1e18);
-    // }
-    // function _convertToShares(uint256 assets) internal view override returns (uint256 shares) {
-    //     shares = assets * IERC4626(MC.YNETH).convertToAssets(1e18) / IERC4626(MC.YNLSDE).convertToAssets(1e18);
-    // }
+    function _deployFunds(uint256 _amount) internal override{
+        // TODO: implement
+    }
+    function _freeFunds(uint256 _amount) internal override {
+        // TODO: implement
+    }
+    function _harvestAndReport() internal override  returns (uint256 _totalAssets){
+        // TODO: implement
+    }
+
+
 }
