f46205dd07ff036fe848f98d511296723d54b82f
Dan OneTree <dan13ram@gmail.com>
2025-01-24 19:54:27 +0530

    added buffer deposit/withdraw tests

--- test/mainnet/tokenizedlpstrategy.t.sol
+++ test/mainnet/tokenizedlpstrategy.t.sol
@@ -90,4 +90,119 @@ contract TokenizedLPStrategyUnitTest is Test, MainnetActors, Etches {
         vm.expectRevert(Provider.RateIsStale.selector);
         vault.processAccounting();
     }
+
+    function allocateToBuffer(uint256 amount) public {
+        address[] memory targets = new address[](2);
+        targets[0] = MC.WETH;
+        targets[1] = MC.BUFFER;
+
+        uint256[] memory values = new uint256[](2);
+        values[0] = 0;
+        values[1] = 0;
+
+        bytes[] memory data = new bytes[](2);
+        data[0] = abi.encodeWithSignature("approve(address,uint256)", vault.buffer(), amount);
+        data[1] = abi.encodeWithSignature("deposit(uint256,address)", amount, address(vault));
+
+        vm.prank(ADMIN);
+        vault.processor(targets, values, data);
+
+        vault.processAccounting();
+    }
+
+    function totalSupplyInvariant(uint256 supply) public view {
+        uint256 finalVaultTotalSupply = vault.totalSupply();
+        assertApproxEqRel(
+            supply, finalVaultTotalSupply, 1e15, "Vault totalSupply should be original totalSupply plus additional"
+        );
+    }
+
+    function totalAssetsInvariant(uint256 assets) public view {
+        uint256 finalVaultTotalAssets = vault.totalAssets();
+        assertApproxEqRel(
+            assets, finalVaultTotalAssets, 1e15, "Vault totalAssets should be original totalAssets plus additional"
+        );
+    }
+
+    function test_deposit_allocateToBuffer_withdraw(uint256 assets, uint256 bufferAmount) public {
+        vm.assume(assets > 10000 && assets < 100_000_000 ether);
+        vm.assume(bufferAmount > 1000 && bufferAmount < assets);
+
+        uint256 initialAssets = vault.totalAssets();
+        uint256 initialSupply = vault.totalSupply();
+
+        // Test the decimals function
+        assertEq(vault.decimals(), 18, "Decimals should be 18");
+
+        // Test the asset function
+        assertEq(vault.asset(), MC.WETH, "Asset address should be WETH");
+
+        // Test the totalAssets function
+        assertGt(vault.totalAssets(), 0, "Total assets should be greater than 0");
+
+        // Test the convertToShares function
+        uint256 shares = vault.convertToShares(assets);
+        assertGt(shares, 0, "Shares should be greater than 0");
+
+        assertApproxEqRel(
+            vault.convertToAssets(shares), assets, 1e15, "Converted assets should equal the original assets"
+        );
+
+        uint256 previewedShares = vault.previewDeposit(assets);
+        assertApproxEqRel(previewedShares, shares, 1e15, "Previewed shares should equal the converted shares");
+
+        // Test the previewMint function
+        uint256 previewedAssets = vault.previewMint(shares);
+        assertApproxEqRel(previewedAssets, assets, 1e15, "Previewed assets should equal the original assets");
+
+        {
+            deal(MC.WETH, address(this), assets);
+            IERC20(MC.WETH).approve(address(vault), assets);
+
+            // Test the deposit function
+            uint256 depositedShares = vault.deposit(assets, address(this));
+            assertEq(depositedShares, shares, "Deposited shares should equal the converted shares");
+        }
+
+        totalSupplyInvariant(initialSupply + shares);
+        totalAssetsInvariant(initialAssets + assets);
+
+        {
+            uint256 balanceBefore = IERC20(MC.WETH).balanceOf(address(vault));
+            uint256 bufferBefore = IERC20(MC.WETH).balanceOf(vault.buffer());
+
+            // allocate to buffer
+            allocateToBuffer(bufferAmount);
+
+            uint256 balanceAfter = IERC20(MC.WETH).balanceOf(address(vault));
+            uint256 bufferAfter = IERC20(MC.WETH).balanceOf(vault.buffer());
+            assertEq(balanceBefore - balanceAfter, bufferAmount, "WETH balance should decrease by buffer amount");
+            assertEq(bufferAfter - bufferBefore, bufferAmount, "Buffer balance should increase by buffer amount");
+        }
+
+        totalSupplyInvariant(initialSupply + shares);
+        totalAssetsInvariant(initialAssets + assets);
+
+        {
+            uint256 maxWithdraw = vault.maxWithdraw(address(this));
+            assertGt(maxWithdraw, 0, "Max withdraw should be greater than 0");
+
+            uint256 balanceBefore = IERC20(MC.WETH).balanceOf(address(vault));
+            uint256 bufferBefore = IERC20(MC.WETH).balanceOf(vault.buffer());
+
+            uint256 convertedShares = vault.convertToShares(maxWithdraw);
+
+            // Test the withdraw function
+            uint256 withdrawnShares = vault.withdraw(maxWithdraw, address(this), address(this));
+            assertApproxEqRel(
+                withdrawnShares, convertedShares, 1e15, "Withdrawn shares should equal the converted shares"
+            );
+
+            uint256 balanceAfter = IERC20(MC.WETH).balanceOf(address(vault));
+            uint256 bufferAfter = IERC20(MC.WETH).balanceOf(vault.buffer());
+
+            assertEq(balanceBefore, balanceAfter, "WETH balance should not change");
+            assertEq(bufferAfter, bufferBefore - maxWithdraw, "Buffer balance should increase by buffer amount");
+        }
+    }
 }
