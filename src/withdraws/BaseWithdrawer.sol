b960e1ca9a0a26bba7f80362aca9a9c8f135a869
Dan OneTree <dan13ram@gmail.com>
2025-01-26 12:30:02 +0530

    re-added async withdrawal lib

--- src/withdraws/BaseWithdrawer.sol
+++ src/withdraws/BaseWithdrawer.sol
@@ -2,9 +2,40 @@
 pragma solidity ^0.8.24;
 
 import {BaseStrategy} from "src/strategy/BaseStrategy.sol";
-import {IVault, VaultLib} from "src/library/VaultLib.sol";
+import {AsyncWithdrawalLib} from "src/library/AsyncWithdrawalLib.sol";
 
 abstract contract BaseWithdrawer is BaseStrategy {
+    function initialize(
+        address admin,
+        string memory name,
+        string memory symbol,
+        uint8 decimals_,
+        bool countNativeAsset_,
+        bool alwaysComputeTotalAssets_
+    ) external virtual initializer {
+        _initialize(admin, name, symbol, decimals_, countNativeAsset_, alwaysComputeTotalAssets_);
+    }
+
+    function _initialize(
+        address admin,
+        string memory name,
+        string memory symbol,
+        uint8 decimals_,
+        bool countNativeAsset_,
+        bool alwaysComputeTotalAssets_
+    ) internal virtual {
+        __ERC20_init(name, symbol);
+        __AccessControl_init();
+        __ReentrancyGuard_init();
+        _grantRole(DEFAULT_ADMIN_ROLE, admin);
+
+        VaultStorage storage vaultStorage = _getVaultStorage();
+        vaultStorage.paused = true;
+        vaultStorage.decimals = decimals_;
+        vaultStorage.countNativeAsset = countNativeAsset_;
+        vaultStorage.alwaysComputeTotalAssets = alwaysComputeTotalAssets_;
+    }
+
     function _feeOnRaw(uint256) public pure override returns (uint256) {
         return 0;
     }
@@ -14,18 +45,7 @@ abstract contract BaseWithdrawer is BaseStrategy {
     }
 
     function computeTotalAssets() public view virtual override returns (uint256 totalBaseBalance) {
-        totalBaseBalance = VaultLib.computeTotalAssets();
-
-        // Get storage
-        IVault.AssetStorage storage assetStorage = VaultLib.getAssetStorage();
-
-        // Iterate through assets
-        address[] memory assetList = assetStorage.list;
-        uint256 assetListLength = assetList.length;
-
-        for (uint256 i = 0; i < assetListLength; i++) {
-            totalBaseBalance += asyncWithdrawalBalance(assetList[i]);
-        }
+        totalBaseBalance = AsyncWithdrawalLib.computeTotalAssets();
     }
 
     /**
--- src/withdraws/Withdrawer.sol
+++ src/withdraws/Withdrawer.sol
@@ -3,112 +3,16 @@ pragma solidity ^0.8.24;
 
 import {BaseWithdrawer} from "src/withdraws/BaseWithdrawer.sol";
 import {OriginWithdrawalLib} from "src/library/OriginWithdrawalLib.sol";
-import {MainnetContracts as MC} from "script/Contracts.sol";
-import {IWithdrawalQueueManager} from "src/interface/IWithdrawalQueueManager.sol";
-import {IWithdrawalQueue} from "src/interface/external/lido/IWithdrawalQueue.sol";
-import {IOETHVault} from "src/interface/external/origin/IOETHVault.sol";
+import {AsyncWithdrawalLib} from "src/library/AsyncWithdrawalLib.sol";
 
 contract Withdrawer is BaseWithdrawer {
-    function initialize(
-        address admin,
-        string memory name,
-        string memory symbol,
-        uint8 decimals_,
-        bool countNativeAsset_,
-        bool alwaysComputeTotalAssets_
-    ) external virtual initializer {
-        _initialize(admin, name, symbol, decimals_, countNativeAsset_, alwaysComputeTotalAssets_);
-    }
-
-    function _initialize(
-        address admin,
-        string memory name,
-        string memory symbol,
-        uint8 decimals_,
-        bool countNativeAsset_,
-        bool alwaysComputeTotalAssets_
-    ) internal virtual {
-        __ERC20_init(name, symbol);
-        __AccessControl_init();
-        __ReentrancyGuard_init();
-        _grantRole(DEFAULT_ADMIN_ROLE, admin);
-
-        VaultStorage storage vaultStorage = _getVaultStorage();
-        vaultStorage.paused = true;
-        vaultStorage.decimals = decimals_;
-        vaultStorage.countNativeAsset = countNativeAsset_;
-        vaultStorage.alwaysComputeTotalAssets = alwaysComputeTotalAssets_;
-    }
-
     /**
      * @notice function to handle the assets that are in queue for withdrawal.
      * @param asset_ The address of the asset.
      * @dev This function should return the amount in base denomination.
      */
-    function asyncWithdrawalBalance(address asset_) public view virtual override returns (uint256 baseAssets) {
-        baseAssets = _asyncWithdrawalBalance(asset_);
-    }
-
-    function _asyncWithdrawalBalanceYNAsset(address queueManager_, address asset_)
-        private
-        view
-        returns (uint256 baseAssets)
-    {
-        IWithdrawalQueueManager queueManager = IWithdrawalQueueManager(queueManager_);
-        (, IWithdrawalQueueManager.WithdrawalRequest[] memory requests) =
-            queueManager.withdrawalRequestsForOwner(address(this));
-
-        uint256 decimals = 10 ** _getAssetStorage().assets[asset_].decimals;
-
-        for (uint256 i = 0; i < requests.length; i++) {
-            if (!requests[i].processed) {
-                // NOTE: needs to be fixed - assumes no slashing for now,
-                // as in reality eigenlayer slashing is not active yet
-                uint256 baseAmount = requests[i].amount * requests[i].redemptionRateAtRequestTime / decimals;
-                uint256 fee = baseAmount * requests[i].feeAtRequestTime / 1000000;
-                baseAssets += baseAmount - fee;
-            }
-        }
-        return baseAssets;
-    }
-
-    function _asyncWithdrawalBalanceWSTETH() private view returns (uint256 baseAssets) {
-        IWithdrawalQueue queue = IWithdrawalQueue(MC.WSTETH_WITHDRAWAL_QUEUE);
-        uint256[] memory requestIds = queue.getWithdrawalRequests(address(this));
-        IWithdrawalQueue.WithdrawalRequestStatus[] memory statuses = queue.getWithdrawalStatus(requestIds);
-        for (uint256 i = 0; i < statuses.length; i++) {
-            baseAssets += statuses[i].amountOfStETH;
-        }
-    }
-
-    function _asyncWithdrawalBalanceWOETH() private view returns (uint256 baseAssets) {
-        IOETHVault oethVault = IOETHVault(MC.OETH_VAULT);
-        uint256[] memory requestIds = OriginWithdrawalLib.getWOETHRequestIds();
-        for (uint256 i = 0; i < requestIds.length; i++) {
-            uint256 requestId = requestIds[i];
-            IOETHVault.WithdrawalRequest memory request = oethVault.withdrawalRequests(requestId);
-            baseAssets += request.amount;
-        }
-    }
-
-    function _asyncWithdrawalBalance(address asset) private view returns (uint256 baseAssets) {
-        if (asset == MC.WOETH) {
-            return _asyncWithdrawalBalanceWOETH();
-        }
-
-        if (asset == MC.WSTETH) {
-            return _asyncWithdrawalBalanceWSTETH();
-        }
-
-        if (asset == MC.YNETH) {
-            return _asyncWithdrawalBalanceYNAsset(MC.YNETH_WITHDRAWAL_QUEUE_MANAGER, MC.YNETH);
-        }
-
-        if (asset == MC.YNLSDE) {
-            return _asyncWithdrawalBalanceYNAsset(MC.YNLSDE_WITHDRAWAL_QUEUE_MANAGER, MC.YNLSDE);
-        }
-
-        return 0;
+    function asyncWithdrawalBalance(address asset_) public view virtual override returns (uint256) {
+        return AsyncWithdrawalLib.asyncWithdrawalBalance(asset_);
     }
 
     function getWOETHRequestIds() external view returns (uint256[] memory) {
