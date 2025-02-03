715ef3b63ceecd4856338ca85fc411faae3da9b0
MrDeadCe11 <mister.deadce11@gmail.com>
2025-02-03 17:00:38 -0600

    removed extra function in IVault>

--- src/interface/IVault.sol
+++ src/interface/IVault.sol
@@ -109,9 +109,6 @@ interface IVault is IERC4626 {
     function getAssets() external view returns (address[] memory list);
     function getAsset(address asset_) external view returns (AssetParams memory);
     function getProcessorRule(address contractAddress, bytes4 funcSig) external view returns (FunctionRule memory);
-    function setProcessorRule(address target, bytes4 functionSig, FunctionRule memory rule) external;
-    function setProcessorRules(address[] memory targets, bytes4[] memory functionSigs, FunctionRule[] memory rules)
-        external;
     function previewDepositAsset(address assetAddress, uint256 assets) external view returns (uint256);
     function depositAsset(address assetAddress, uint256 amount, address receiver) external returns (uint256);
     function provider() external view returns (address);
@@ -121,7 +118,7 @@ interface IVault is IERC4626 {
     function setProvider(address provider) external;
     function setBuffer(address buffer) external;
     function setProcessorRule(address target, bytes4 functionSig, FunctionRule memory rule) external;
-
+    function setProcessorRules(address[] memory targets, bytes4[] memory functionSigs, FunctionRule[] memory rules) external;
     function addAsset(address asset_, bool active_) external;
     function pause() external;
     function unpause() external;
