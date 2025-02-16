9fa03938d3065a72d12453fe29b200f74342e153
danoctavian <danoctavian91@gmail.com>
2025-02-16 12:53:34 +0200

    add test for ynETH and ynLSDe deposit

--- test/mainnet/configurer.spec.sol
+++ test/mainnet/configurer.spec.sol
@@ -22,6 +22,8 @@ import {BaseVault} from "src/BaseVault.sol";
 import {IynETH} from "test/interface/external/yieldnest/IynETH.sol";
 import {IWETH} from "test/interface/external/ethereum/IWETH.sol";
 import {IERC4626} from "lib/openzeppelin-contracts/contracts/interfaces/IERC4626.sol";
+import {ICurveLpConnector} from "src/interface/ICurveLpConnector.sol";
+
 
 contract VaultConfigureUpgradeTest is Test, MainnetActors, AssertUtils {
     Vault public vault;
@@ -635,5 +637,65 @@ contract VaultConfigureUpgradeTest is Test, MainnetActors, AssertUtils {
             1e14, // withdrawal fee precision error is at 0.001% of amount
             "Vault total assets should include withdrawal fee"
         );
+    }
+
+    function testDepositYnETHAndYnLSDeToConnector() public {
+        uint256 depositAmount = 1e18;
+        uint256 vaultTotalAssetsBefore = vault.totalAssets();
+
+        address alice = makeAddr("alice");
+        {
+            deal(MC.YNETH, alice, depositAmount);
+            deal(MC.YNLSDE, alice, depositAmount);
+
+            // Alice deposits equal amounts of ynETH and ynLSDe
+            vm.startPrank(alice);
+            IERC20(MC.YNETH).approve(address(vault), depositAmount);
+            IERC20(MC.YNLSDE).approve(address(vault), depositAmount);
+            vault.depositAsset(MC.YNETH, depositAmount, alice);
+            vault.depositAsset(MC.YNLSDE, depositAmount, alice);
+            vm.stopPrank();
+
+            vault.processAccounting();
+        }
+
+        // Record total assets after deposits but before connector deposit
+        uint256 totalAssetsAfterDeposits = vault.totalAssets();
+
+
+        // Deposit equal amounts to ynETH and ynLSDe
+        {
+            address[] memory targets = new address[](4);
+            uint256[] memory values = new uint256[](4);
+            bytes[] memory data = new bytes[](4);
+
+            // Approve and deposit to ynETH
+            targets[0] = MC.YNETH;
+            values[0] = 0;
+            data[0] = abi.encodeCall(IERC20.approve, (MC.CURVE_LP_YNETH_YNLSDE_CONNECTOR, depositAmount));
+
+            targets[1] = MC.YNLSDE;
+            values[1] = 0;
+            data[1] = abi.encodeCall(IERC20.approve, (MC.CURVE_LP_YNETH_YNLSDE_CONNECTOR, depositAmount));
+
+            // Deposit to connector
+            targets[2] = MC.CURVE_LP_YNETH_YNLSDE_CONNECTOR;
+            values[2] = 0;
+            data[2] = abi.encodeCall(ICurveLpConnector.deposit, (depositAmount, depositAmount, 0));
+
+            vm.startPrank(PROCESSOR);
+            vault.processor(targets, values, data);
+            vm.stopPrank();
+        }
+
+        // // Process accounting
+        // vault.processAccounting();
+
+        // // Verify balances
+        // assertEq(
+        //     vault.totalAssets(),
+        //     vaultTotalAssetsBefore + depositAmount,
+        //     "Vault total assets should increase by deposit amount"
+        // );
     }
 }
