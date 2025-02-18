b95a15e722d8bafa6f17fb3ad790645538f143b9
danoctavian <danoctavian91@gmail.com>
2025-02-18 03:30:05 +0200

    increase bound

--- test/mainnet/configurer.spec.sol
+++ test/mainnet/configurer.spec.sol
@@ -294,7 +294,7 @@ contract VaultConfigureUpgradeTest is Test, MainnetActors, AssertUtils {
         uint256 rate = IProvider(vault.provider()).getRate(asset);
         uint256 baseAmount = Math.mulDiv(donatedAmount, rate, 10 ** 18, Math.Rounding.Floor);
 
-        assertApproxEqRel(vault.totalAssets(), totalAssetBefore + baseAmount, 1e8, "Total assets should be correct");
+        assertApproxEqRel(vault.totalAssets(), totalAssetBefore + baseAmount, 1e10, "Total assets should be correct");
     }
 
     function test_deposit_any_asset(uint256 depositAmount, uint8 assetIndex) public {
