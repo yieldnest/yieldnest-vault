c5dcd38795687a85566a82a9cdc406074bbdc8a3
danoctavian <danoctavian91@gmail.com>
2025-02-19 11:40:48 +0200

    adjust 100 ether expression

--- test/mainnet/helpers/TestHelper.sol
+++ test/mainnet/helpers/TestHelper.sol
@@ -92,7 +92,7 @@ contract TestHelper is Test {
             // 1e4 in 1e18 is 0 for a < 1e14
             // threshold is 1 wei for a < 1e14
             assertApproxEqAbs(a, b, 1, message);
-        } else if (a < 1e2 ether) {
+        } else if (a < 100 ether) {
             // 1e4 / 1e18 = 1e-14 (= 1e-12 percent)
             // 1e4 / 1e18 at 1e14 wei = 1 wei (lower bound)
             // 1e4 / 1e18 at 1e2 ether (or 1e20 wei) = 1e6 wei (higher bound)
