3cdff646764088670973068f6d83da1dc2da60d8
danoctavian <danoctavian91@gmail.com>
2025-03-29 12:18:10 +0200

    add decimals explanation

--- src/library/VaultLib.sol
+++ src/library/VaultLib.sol
@@ -211,8 +211,14 @@ library VaultLib {
         if (totalAssets == 0 || totalSupply == 0) {
             IVault.VaultStorage storage vaultStorage = getVaultStorage();
             address baseAsset = getAssetStorage().list[0];
+            
+            //// Base Case Mint ////
+            //Mints 1 Unit of Vault Shares for 1 Unit of baseAsset
+            // Example: if the Vault has 18 decimals and the baseAsset has 6 decimals,
+            // mint 1e18 shares for 1e6 of baseAsset
+            uint256 baseAssetsOffset = 10 ** (vaultStorage.decimals - IERC20Metadata(baseAsset).decimals());
             baseAssets =
-                shares.mulDiv(1, 10 ** (vaultStorage.decimals - IERC20Metadata(baseAsset).decimals()), rounding);
+                shares.mulDiv(1, baseAssetsOffset, rounding);
         } else {
             baseAssets = shares.mulDiv(totalAssets + 1, totalSupply + 1, rounding);
         }
@@ -237,16 +243,27 @@ library VaultLib {
 
         IVault.VaultStorage storage vaultStorage = getVaultStorage();
         address baseAsset = getAssetStorage().list[0];
+
+        // Offset applied to baseAsset amounts to match the Vault's decimals, to preserve precision
         uint256 baseAssetsOffset = 10 ** (vaultStorage.decimals - IERC20Metadata(baseAsset).decimals());
 
+        // value of assets (in asset_) converted to baseAsset with the offset applied
         uint256 baseAssetsWithOffset = convertAssetToBase(asset_, assets * baseAssetsOffset);
+
+        // amount of baseAssets
         uint256 baseAssets = baseAssetsWithOffset.mulDiv(1, baseAssetsOffset, rounding);
 
         // Handle the case when totalAssets or totalSupply is zero
         if (totalAssets == 0 || totalSupply == 0) {
+            //// Base Case Mint ////
+            //Mints 1 Unit of Vault Shares for 1 Unit of baseAsset
+            // Example: if the Vault has 18 decimals and the baseAsset has 6 decimals,
+            // mint 1e18 shares for 1e6 of baseAsset
             return (baseAssetsWithOffset, baseAssets);
         }
 
+        // amount of shares calculated using baseAssetsWithOffset to preserve precision
+        // dividing by baseAssetsOffset to convert back to vault decimals
         uint256 shares = baseAssetsWithOffset.mulDiv(totalSupply + 1, totalAssets * baseAssetsOffset + 1, rounding);
         return (shares, baseAssets);
     }
