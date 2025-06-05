e4369d20ea5778d12ecfe4055c8713274b6f5a18
yield-farmer-1 <parth@yieldnest.finance>
2025-06-05 18:37:50 +0530

    add test for Provider

---
+++ test/mainnet/provider.spec.sol
@@ -0,0 +1,78 @@
+// SPDX-License-Identifier: BSD Clause-3
+pragma solidity ^0.8.24;
+
+import {BaseTest} from "test/mainnet/helpers/BaseTest.sol";
+import {IERC4626} from "src/Common.sol";
+import {BufferStrategy} from "src/BufferStrategy.sol";
+import {Provider} from "src/module/Provider.sol";
+import {MainnetContracts as MC} from "script/Contracts.sol";
+
+contract ProviderTest is BaseTest {
+
+    Provider public provider;
+    BufferStrategy public bufferStrategy;
+
+    function setUp() public {
+        (, bufferStrategy, provider) = BaseTest.deploy();
+    }
+
+    function test_getRate_Of_WrappedUSDC() public {
+        assertEq(provider.getRate(address(wrappedUSDC)), 1e18);
+    }
+
+        function test_getRate_Of_USDC() public {
+        assertEq(provider.getRate(MC.USDC), 1e18);
+    }
+
+    function test_getRate_Of_USDT() public {
+        assertEq(provider.getRate(MC.USDT), 1e18);
+    }
+
+    function test_getRate_Of_GHO() public {
+        assertEq(provider.getRate(MC.GHO), 1e18);
+    }
+
+    function test_getRate_Of_USDE() public {
+        assertEq(provider.getRate(MC.USDE), 1e18);
+    }
+
+    function test_getRate_Of_CRVUSD() public {
+        assertEq(provider.getRate(MC.CRVUSD), 1e18);
+    }
+
+    function test_getRate_Of_USDS() public {
+        assertEq(provider.getRate(MC.USDS), 1e18);
+    }
+
+    function test_getRate_Of_FRAX() public {
+        assertEq(provider.getRate(MC.FRAX), 1e18);
+    }
+
+    function test_getRate_Of_SFRAX() public {
+        assertEq(provider.getRate(MC.SFRAX), IERC4626(MC.SFRAX).convertToAssets(1e18));
+    }
+
+    function test_getRate_Of_SUSDE() public {
+        assertEq(provider.getRate(MC.SUSDE), IERC4626(MC.SUSDE).convertToAssets(1e18));
+    }
+
+    function test_getRate_Of_SUSDS() public {
+        assertEq(provider.getRate(MC.SUSDS), IERC4626(MC.SUSDS).convertToAssets(1e18));
+    }
+
+    function test_getRate_Of_SCRVUSD() public {
+        assertEq(provider.getRate(MC.SCRVUSD), IERC4626(MC.SCRVUSD).convertToAssets(1e18));
+    }
+
+    function test_getRate_Of_SuperUSDCVault() public {
+        assertEq(provider.getRate(MC.SUPER_USDC_VAULT), IERC4626(MC.SUPER_USDC_VAULT).convertToAssets(1e18));
+    }
+
+    function test_getRate_Of_MorphoGauntletUSDCVault() public {
+        assertEq(provider.getRate(MC.MORPHO_GAUNTLET_USDC_VAULT), IERC4626(MC.MORPHO_GAUNTLET_USDC_VAULT).convertToAssets(1e18) * 1e12);
+    }
+
+    function test_getRate_Of_USDStrategyVault() public {
+        assertEq(provider.getRate(address(bufferStrategy)), IERC4626(bufferStrategy).convertToAssets(1e18) * 1e12);
+    }
+}
