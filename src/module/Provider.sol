3d8194860198803ff050bf597aa2e953c8831547
danoctavian <danoctavian91@gmail.com>
2025-06-08 13:41:04 +0300

    wip test updates

--- src/module/Provider.sol
+++ src/module/Provider.sol
@@ -28,84 +28,9 @@ contract Provider is IProvider {
     error UnsupportedAsset(address asset);
     error RateIsNegative();
 
-    function isETHStrategyVault(address asset) public view returns (bool) {
-        try IBaseStrategy(asset).STRATEGY_VERSION() returns (string memory version) {
-            address vaultAsset = IVault(asset).asset();
-            return (
-                keccak256(bytes(version)) == keccak256(bytes("0.1.0"))
-                    || keccak256(bytes(version)) == keccak256(bytes("0.2.0"))
-            ) && vaultAsset == MC.WETH;
-        } catch {
-            return false;
-        }
-    }
-
     function getRate(address asset) public view virtual returns (uint256) {
-        if (asset == MC.WETH) {
+        if (asset == MC.USDC) {
             return 1e18;
-        }
-
-        if (asset == MC.STETH) {
-            return 1e18;
-        }
-
-        if (asset == MC.OETH) {
-            return 1e18;
-        }
-
-        if (asset == MC.BUFFER || asset == MC.YNETH || asset == MC.WOETH || asset == MC.EULER_WETH_22_VAULT) {
-            return IERC4626(asset).convertToAssets(1e18);
-        }
-
-        if (asset == MC.YNLSDE) {
-            // ynLSDe does not expose convertToAssets, use previewRedeem instead
-            return IynLSDe(asset).previewRedeem(1e18);
-        }
-
-        if (asset == MC.SMOKEHOUSE_WSTETH) {
-            return IStETH(MC.STETH).getPooledEthByShares(IERC4626(asset).convertToAssets(1e18));
-        }
-
-        if (asset == MC.WSTETH) {
-            return IStETH(MC.STETH).getPooledEthByShares(1e18);
-        }
-
-        if (asset == MC.METH) {
-            return IMETH(MC.METH_STAKING_MANAGER).mETHToETH(1e18);
-        }
-
-        if (asset == MC.RETH) {
-            return IRETH(MC.RETH).getExchangeRate();
-        }
-
-        if (asset == MC.SWELL) {
-            return IswETH(MC.SWELL).swETHToETHRate();
-        }
-
-        if (asset == MC.SFRXETH) {
-            /* 
-            
-            The deposit asset for sfrxETH is frxETH and not ETH. 
-            In order to account for any frxETH/ETH rate fluctuations,
-            an frxETH/ETH oracle is used as provided by Frax.
-
-            Documentation: https://docs.frax.finance/frax-oracle/advanced-concepts
-            */
-            uint256 frxETHPriceInETH = IFrxEthWethDualOracle(MC.FRX_ETH_WETH_DUAL_ORACLE).getCurveEmaEthPerFrxEth();
-            return IsfrxETH(MC.SFRXETH).pricePerShare() * frxETHPriceInETH / 1e18;
-        }
-
-        if (asset == MC.CURVE_LP_YNETH_YNLSDE_STRATEGY) {
-            (int256 strategyRate,) = ICurveLpConnector(MC.CURVE_LP_YNETH_YNLSDE_CONNECTOR).rate();
-            if (strategyRate < 0) {
-                revert RateIsNegative();
-            }
-
-            return uint256(strategyRate);
-        }
-
-        if (isETHStrategyVault(asset)) {
-            return IERC4626(asset).convertToAssets(1e18);
         }
 
         revert UnsupportedAsset(asset);
--- test/mainnet/BaseIntegrationTest.sol
+++ test/mainnet/BaseIntegrationTest.sol
@@ -19,11 +19,66 @@ import {Withdrawer} from "src/withdraws/Withdrawer.sol";
 import {ProxyAdmin} from "@openzeppelin/contracts/proxy/transparent/ProxyAdmin.sol";
 import {ITransparentUpgradeableProxy} from "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
 import {ProxyUtils} from "script/ProxyUtils.sol";
+import {WrappedToken} from "lib/wrapped-token/src/WrappedToken.sol";
+import {TransparentUpgradeableProxy as TUProxy} from "src/Common.sol";
+import {PublicViewsVault} from "test/unit/helpers/PublicViewsVault.sol";
 
 contract BaseIntegrationTest is Test, MainnetActors, AssertUtils {
     Vault public vault;
+    WrappedToken public wusdc;
 
     function setUp() public virtual {
-        vault = Vault(payable(MC.YNETHX));
+                string memory name = "YieldNest RWA MAX";
+        string memory symbol = "ynRWAx";
+
+        Vault vaultImplementation = new PublicViewsVault();
+
+        // Deploy the proxy
+        TUProxy vaultProxy = new TUProxy(address(vaultImplementation), ADMIN, "");
+
+        vault = Vault(payable(address(vaultProxy)));
+
+        // Initialize the vault
+        // Initialize the vault with the following parameters:
+        // ADMIN: The address that will have admin privileges
+        // name: The name of the vault token ("YieldNest RWA MAX")
+        // symbol: The symbol of the vault token ("ynRWAx")
+        // 18: The number of decimals for the vault token
+        // 0: The withdrawal fee in basis points (0 = no fee)
+        // false: Whether to count native assets (ETH) in the vault
+        // true: Whether to always compute total assets (instead of tracking incrementally)
+        // 1: The default asset index to use (the second asset added will be default)
+        vault.initialize(ADMIN, name, symbol, 18, 0, false, true, 1);
+
+
+        wusdc = WrappedToken(address(new TUProxy(address(new WrappedToken()), ADMIN, "")));
+        wusdc.initialize(IERC20(MC.USDC), "Wrapped USDC", "wUSDC", 18, 12);
+
+        configureMainnet(vault);
+    } 
+
+    function configureMainnet(Vault vault) internal {
+
+        vm.startPrank(ADMIN);
+
+        // Grant roles
+        vault.grantRole(vault.PROCESSOR_ROLE(), PROCESSOR);
+        vault.grantRole(vault.PROVIDER_MANAGER_ROLE(), PROVIDER_MANAGER);
+        vault.grantRole(vault.BUFFER_MANAGER_ROLE(), BUFFER_MANAGER);
+        vault.grantRole(vault.ASSET_MANAGER_ROLE(), ASSET_MANAGER);
+        vault.grantRole(vault.PROCESSOR_MANAGER_ROLE(), PROCESSOR_MANAGER);
+        vault.grantRole(vault.PAUSER_ROLE(), PAUSER);
+        vault.grantRole(vault.UNPAUSER_ROLE(), UNPAUSER);
+        vault.grantRole(vault.FEE_MANAGER_ROLE(), FEE_MANAGER);
+
+        Provider provider = new Provider();
+        // Set the provider to the 6 decimals provider
+        vault.setProvider(address(provider));
+
+        // Add assets: Base asset (USDC) first, then WBTC and an 18 decimal asset
+
+        vault.addAsset(address(wusdc), true);
+        vault.addAsset(MC.USDC, true); // USDC mocked at WETH address
+        vault.addAsset(MC.BUFFER, false);
     }
 }
