d7a2ea79fb008800a56ec747867dc9c41baf63bd
yield-farmer-1 <parth@yieldnest.finance>
2025-07-22 11:43:43 +0530

    refactor PR comments

--- src/FeeModule.sol
+++ src/FeeModule.sol
@@ -6,19 +6,17 @@ import {IVault} from "src/interface/IVault.sol";
 import {YnETHx} from "src/YnETHx.sol";
 import {IFeeModule} from "src/interface/IFeeModule.sol";
 
-contract YnETHxFeeModule is OwnableUpgradeable, IFeeModule {
-    // performance deniminated in ether(i.e. 1e18 = 100%)
+contract FeeModule is OwnableUpgradeable, IFeeModule {
+    // performance denominated in ether(i.e. 1e18 = 100%)
     uint256 public performanceFee;
     address public performanceFeeRecipient;
-    YnETHx public immutable YNETHX;
-    uint256 public immutable YNETHX_DECIMALS;
+    IVault public immutable VAULT;
+    uint256 public immutable VAULT_DECIMALS;
     uint256 public maximumAccountedExchangeRate;
 
-    event PerformanceFeeCharged(address indexed recipient, uint256 sharesMinted, uint256 performanceFeeAmount);
-
-    constructor(address ynETHx_) {
-        YNETHX = YnETHx(payable(ynETHx_));
-        YNETHX_DECIMALS = ERC20(address(YNETHX)).decimals();
+    constructor(address vault_) {
+        VAULT = IVault(payable(vault_));
+        VAULT_DECIMALS = ERC20(address(VAULT)).decimals();
     }
 
     function initialize(address owner_, uint256 performanceFee_, address performanceFeeRecipient_)
@@ -29,27 +27,27 @@ contract YnETHxFeeModule is OwnableUpgradeable, IFeeModule {
         if (performanceFee_ > 1 ether) revert InvalidPerformanceFee();
         performanceFee = performanceFee_;
         performanceFeeRecipient = performanceFeeRecipient_;
-        maximumAccountedExchangeRate = IERC4626(address(YNETHX)).convertToAssets(1 ether);
+        maximumAccountedExchangeRate = IERC4626(address(VAULT)).convertToAssets(10 ** VAULT_DECIMALS);
     }
 
     function chargePerformanceFee() external {
-        if (msg.sender != address(YNETHX)) revert CallerNotVault();
+        if (msg.sender != address(VAULT)) revert CallerNotVault();
 
-        uint256 exchangeRateBeforeFee = IERC4626(address(YNETHX)).convertToAssets(1 ether);
+        uint256 exchangeRateBeforeFee = IERC4626(address(VAULT)).convertToAssets(10 ** VAULT_DECIMALS);
 
         if (exchangeRateBeforeFee > maximumAccountedExchangeRate) {
-            uint256 totalSupplyBeforeFee = IERC4626(address(YNETHX)).totalSupply();
+            uint256 totalSupplyBeforeFee = IERC4626(address(VAULT)).totalSupply();
             uint256 yieldEarned =
-                (exchangeRateBeforeFee - maximumAccountedExchangeRate) * totalSupplyBeforeFee / (10 ** YNETHX_DECIMALS);
+                (exchangeRateBeforeFee - maximumAccountedExchangeRate) * totalSupplyBeforeFee / (10 ** VAULT_DECIMALS);
             uint256 feesAccrued = (yieldEarned * performanceFee) / 1 ether;
 
             if (performanceFeeRecipient != address(0)) {
-                uint256 sharesToMint = IERC4626(address(YNETHX)).previewDeposit(feesAccrued);
-                YNETHX.mintPerformanceFeeShares(performanceFeeRecipient, sharesToMint);
+                uint256 sharesToMint = IERC4626(address(VAULT)).previewDeposit(feesAccrued);
+                VAULT.mintPerformanceFeeShares(performanceFeeRecipient, sharesToMint);
                 emit PerformanceFeeCharged(performanceFeeRecipient, sharesToMint, feesAccrued);
             }
 
-            uint256 exchangeRateAfterFee = IERC4626(address(YNETHX)).convertToAssets(1 ether);
+            uint256 exchangeRateAfterFee = IERC4626(address(VAULT)).convertToAssets(10 ** VAULT_DECIMALS);
             if (exchangeRateAfterFee > maximumAccountedExchangeRate) {
                 maximumAccountedExchangeRate = exchangeRateAfterFee;
             }
@@ -58,10 +56,12 @@ contract YnETHxFeeModule is OwnableUpgradeable, IFeeModule {
 
     function setPerformanceFee(uint256 performanceFee_) external onlyOwner {
         if (performanceFee_ > 1 ether) revert InvalidPerformanceFee();
+        emit SetPerformanceFee(performanceFee, performanceFee_);
         performanceFee = performanceFee_;
     }
 
     function setPerformanceFeeRecipient(address performanceFeeRecipient_) external onlyOwner {
+        emit SetPerformanceFeeRecipient(performanceFeeRecipient, performanceFeeRecipient_);
         performanceFeeRecipient = performanceFeeRecipient_;
     }
 }
