36bd25b213f4d4ecc0f68a28138c5a2f101bf5d0
danoctavian <danoctavian91@gmail.com>
2025-08-02 20:35:28 +0300

    asset count in tests

--- test/mainnet/upgrade.spec.sol
+++ test/mainnet/upgrade.spec.sol
@@ -83,7 +83,7 @@ contract VaultMainnetUpgradeTest is BaseIntegrationTest {
 
         // Test the getAssets function
         address[] memory assets = vault.getAssets();
-        assertEq(assets.length, 8, "There should be 8 assets in the vault");
+        assertEq(assets.length, 9, "There should be 9 assets in the vault");
     }
 
     function test_Vault_Upgrade_totalAssets_unchanged(bool processAccountingBeforeCheck) public {
