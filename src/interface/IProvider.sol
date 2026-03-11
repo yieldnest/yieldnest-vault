97234827d6e326fd922d9a1f34c96c6068755a87
Sean <sean@yieldnest.finance>
2026-03-11 17:29:49 +0000

    Remove unused

--- src/interface/IProvider.sol
+++ src/interface/IProvider.sol
@@ -37,30 +37,3 @@ interface IynLSDe {
     function convertToAssets(address asset, uint256 shares) external view returns (uint256);
     function previewRedeem(uint256 shares) external view returns (uint256);
 }
-
-interface IAaveV3Pool {
-    function supply(address asset, uint256 amount, address onBehalfOf, uint16 referralCode) external;
-    function withdraw(address asset, uint256 amount, address to) external returns (uint256);
-    function borrow(address asset, uint256 amount, uint256 interestRateMode, uint16 referralCode, address onBehalfOf)
-        external;
-    function repay(address asset, uint256 amount, uint256 interestRateMode, address onBehalfOf)
-        external
-        returns (uint256);
-    function getUserAccountData(address user)
-        external
-        view
-        returns (
-            uint256 totalCollateralBase,
-            uint256 totalDebtBase,
-            uint256 availableBorrowsBase,
-            uint256 currentLiquidationThreshold,
-            uint256 ltv,
-            uint256 healthFactor
-        );
-    function setUserEMode(uint8 categoryId) external;
-}
-
-interface IAaveV3Oracle {
-    function getAssetPrice(address asset) external view returns (uint256);
-    function BASE_CURRENCY_UNIT() external view returns (uint256);
-}
